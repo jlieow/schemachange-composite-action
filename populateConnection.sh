b3479c9ab316770680d64cf817c84f4ed1949c79
jlieow <52845474+jlieow@users.noreply.github.com>
2025-03-04 13:44:23 +0800

    Update populateConnection.sh
    Update populateConnection.sh
    
    Update action.yml
    
    clean up
    
    clean up
    
    Update populateConnection.sh
    
    Update populateConnection.sh

--- populateConnection.sh
+++ populateConnection.sh
@@ -8,7 +8,10 @@ echo role = \"${SF_ROLE}\" >> ./connections.toml
 echo warehouse = \"${SF_WAREHOUSE}\" >> ./connections.toml
 echo database = \"${SF_DATABASE}\" >> ./connections.toml
 echo password = \"${SF_PASSWORD}\" >> ./connections.toml
-echo private_key_file = \"${SF_PRIVATE_KEY_FILE}\" >> ./connections.toml
-echo private_key_file_pwd = \"${SF_PRIVATE_KEY_FILE_PWD}\" >> ./connections.toml
+# If key is provided create key file
+if [[ -n ${SF_PRIVATE_KEY} ]]; then
+  echo private_key_file = \"${SF_PRIVATE_KEY_FILE}\" >> ./connections.toml
+  echo private_key_file_pwd = \"${SF_PRIVATE_KEY_FILE_PWD}\" >> ./connections.toml
+fi
 echo "cat connections.toml"
 cat ./connections.toml
