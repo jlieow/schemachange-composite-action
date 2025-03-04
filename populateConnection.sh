d11fafbe7be9da0d8e129b8969296c8ca127448d
jlieow <52845474+jlieow@users.noreply.github.com>
2025-03-04 09:45:09 +0800

    Added key pair auth fields

--- populateConnection.sh
+++ populateConnection.sh
@@ -8,5 +8,7 @@ echo role = \"${SF_ROLE}\" >> ./connections.toml
 echo warehouse = \"${SF_WAREHOUSE}\" >> ./connections.toml
 echo database = \"${SF_DATABASE}\" >> ./connections.toml
 echo password = \"${SF_PASSWORD}\" >> ./connections.toml
+echo private_key_file = \"${SF_PRIVATE_KEY_FILE}\" >> ./connections.toml
+echo private_key_file_pwd = \"${SF_PRIVATE_KEY_FILE_PWD}\" >> ./connections.toml
 echo "cat connections.toml"
 cat ./connections.toml
